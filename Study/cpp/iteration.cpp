de820612154c32b9e7abff88ae39be3bee549676
장준영 <110384101+junzer0@users.noreply.github.com>
2024-04-24 23:59:40 +0900

    Add files via upload

---
+++ Study/cpp/iteration.cpp
@@ -0,0 +1,20 @@
+//
+// Created by zero on 24. 4. 24.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    cin >> a;
+
+    for(int i = a; i >=0; i--)
+    {
+        cout << i << " ";
+    }
+
+    cout << "!" << endl;
+    return 0;
+}
